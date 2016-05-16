09e1a8652b8a25fff03b05517ff913daa8a6200d
Clay Griswold <ccgriswold@gmail.com>
2016-05-16 11:13:50 -0400

    continued work with the site, firebase currently fails

--- js/factory.js
+++ js/factory.js
@@ -1,10 +1,22 @@
 /* jslint browser: true, esnext: true */
 
 module.exports = (function(){
+
+  // let Firebase = require('firebase');
+  //   let fireRequest = new Firebase('https://griswold-car-auction.firebase.io.com');
+
   let service = angular.module('CarListService', []);
 
-  let Firebase = require('firebase');
-  let fireRequest = new Firebase('https://griswold-car-auction.firebase.io.com/');
+  function Car(title, miles, price, year, image, id){
+        this.title = title;
+        this.id = id;
+        this.miles = miles;
+        this.price = price;
+        this.year = year;
+        this.image = image;
+
+        return this;
+    }
 
   service.factory('CarListService', function($http){
     let carCollection = [];
@@ -21,6 +33,7 @@ module.exports = (function(){
             // carNames.push(response.data);
             angular.copy(response.data, carNames);
             console.log(carNames);
+
           // }
         });
       },
@@ -31,12 +44,16 @@ module.exports = (function(){
           url: 'https://evening-depths-24907.herokuapp.com/cars',
         }).then(function(response){
           console.log(response);
-          for(let i = 0; i < response.data.length < 4; i++){
-            carCollection.push(response.data);
+          for(let i = 0; i < response.data.length; i++){
+            angular.copy(response.data, carCollection);
             // angular.copy(response.data, carNames);
             console.log(carCollection);
+
+            // let savedCar = new Car(title, miles, price, year, image, id);
+            // let carToSave = new Firebase('https://griswold-car-auction.firebaseio.com/savedcars/'+ savedCar.id);
           }
         });
+
       },
 
       getCars: function(){
